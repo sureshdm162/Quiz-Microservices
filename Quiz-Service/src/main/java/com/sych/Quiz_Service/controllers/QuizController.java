cbf3032c6897f2e29fd735e38c6263c48659db83
Suresh D M <sureshdm162@gmail.com>
2025-05-20 08:38:16 +0530

    Committing QuizController.java

--- Quiz-Service/src/main/java/com/sych/Quiz_Service/controllers/QuizController.java
+++ Quiz-Service/src/main/java/com/sych/Quiz_Service/controllers/QuizController.java
@@ -22,27 +22,37 @@ public class QuizController {
     @Autowired
     QuizService quizService;
 
+    //Create a new quiz by passing "title","Num of questions" and "Category like Java, python....etc." in JSON format.
+    // USE URL:  http://localhost:8090/sych/quiz/create
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestBody QuizDto quizDto){
           return quizService.createQuiz(quizDto.getTitle(),quizDto.getNumQuestions(),quizDto.getCategory());
     }
 
+    //Just give the quiz id as input if created earlier, So it returns you the list of questions with only options in JSON format.
+    //USE URL: http://localhost:8090/sych/quiz/getQuizById/your_quiz_Id
     @GetMapping("getQuizById/{id}")
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable int id){
         return quizService.getQuizQuestions(id);
     }
 
+    //Submit the quiz by passing question "id" and "response" in JSON format.
+    //USE URL: http://localhost:8090/sych/quiz/submit/your_quiz_Id
     @PostMapping("submit/{id}")
     public ResponseEntity<Integer> submit(@PathVariable Integer id,@RequestBody List<Response> responses){
         log.info("Response taken by Quiz server at submit()!");
         return quizService.calculateResult(responses);
     }
 
+    //Input all the details of Question class in JSON format to add question.
+    //USE URL: http://localhost:8090/sych/quiz/addQuestion
     @PostMapping("addQuestion")
     public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return quizService.addQuestion(question);
     }
 
+    //Just pass the id in the URL to delete a particular question.
+    //USE URL: http://localhost:8090/sych/quiz/deleteQuestion/Your_Question_Id
     @DeleteMapping("deleteQuestion/{id}")
     public ResponseEntity<String> deleteQuestionById(@PathVariable int id){
         return quizService.deleteQuestionById(id);
